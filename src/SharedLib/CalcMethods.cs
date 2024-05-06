6a5661f6187651753b85f846a3f381c4288f1228
Marcus Bernander <marcus@bernander.se>
2024-05-06 17:55:12 +0200

    Outline of a lab project. One console app One Web app One shared lib One test-project for the shared lib

---
+++ src/SharedLib/CalcMethods.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedLib;
+
+public class CalcMethods
+{
+    public static int IterAdd(int start, int bump, int iterations)
+    {
+        throw new NotImplementedException();
+    }
+}
