ea05877886a157759731ed0d804e894d543a0630
Erikprao <169045306+Erikprao@users.noreply.github.com>
2024-05-07 11:31:40 +0200

    Made a table

---
+++ src/IterAddWeb/Pages/Calc.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IterAddWeb.Pages
+{
+    public class CalcModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
